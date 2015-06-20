8596465d8bea2c5beb851b9e36f60f440f53409f
alex <alex.me.jones@gmail.com>
2015-06-20 14:45:54 +0100

    read by lines and print several variables

--- fun.py
+++ fun.py
@@ -1,10 +1,14 @@
 #!/use/bin/env python3
 
 def find_site(base_seq, site):
-    return print(base_seq.find(site))
+    return (base_seq.find(site))
 
 with open('seq.txt', 'rt') as seq:
     #print(seq.read())
     for line in seq:
-        print (line)
-        find_site (line, 'prot1')
+        line = line.strip()
+        site = find_site (line, '1')
+        length = len(line)
+        print('line is {0}, length is {1}, site position is {2}'.format\
+        (line, length, site), sep = ' ')
+        #print (site)
