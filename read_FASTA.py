4a51e17db61d98ff304b6ab90724a9e376627af9
alex <alex.me.jones@gmail.com>
2015-06-20 23:20:55 +0100

    tidy up and add argv key

--- read_FASTA.py
+++ read_FASTA.py
@@ -1,15 +1,13 @@
 #!/use/bin/env python3
 
-from pprint import pprint
-
+import sys
 
 def read_FASTA_protein(FASTAfile):
     ''' making one def for reading FASTA sequences into a dict with protein name as key'''
     with open (FASTAfile) as in_file:    
         return {part[0]: part[2].replace('\n','') for part in [record.partition('\n') for \
         record in in_file.read().split('>')[1:]]}
-
     
 proteins = read_FASTA_protein('seq.txt')
-#pprint (proteins)
-print (proteins['prot1'])
+#print (proteins)
+print (proteins[sys.argv[1]])
