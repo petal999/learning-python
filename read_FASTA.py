f2f9cad6cc15871ee342d9bb4892ddb0ab470ccc
alex <alex.me.jones@gmail.com>
2015-06-20 20:53:02 +0100

    have three refs to read and split FASTA files

--- read_FASTA.py
+++ read_FASTA.py
@@ -6,15 +6,22 @@ def read_FASTA(FASTAfile):
     ''' creates a tuple for each protein from a FASTA file'''
     with open (FASTAfile) as in_file:
         return (in_file.read().split('>') [1:])
-
-def read_FASTA_split(FASTAfile):
-    return [entry.partition('\n') for entry in read_FASTA(FASTAfile)] 
-
-         
+        
 #create a list from the FASTAfile
 #proteins = (read_FASTA('seq.txt'))
 #print (proteins)
 #print (proteins[1])
 
-protein = read_FASTA_split('seq.txt')
-pprint (protein)
+
+def read_FASTA_split(FASTAfile):
+    '''start using comprehension to split fasta entries '''
+    return [entry.partition('\n') for entry in read_FASTA(FASTAfile)] 
+
+#protein = read_FASTA_split('seq.txt')
+#pprint (protein)
+
+def read_FASTA_protein(FASTAfile):
+    return [[seq[0], seq[2].replace('\n','')] for seq in \
+    read_FASTA_split(FASTAfile)]
+    
+pprint(read_FASTA_protein('seq.txt'))
