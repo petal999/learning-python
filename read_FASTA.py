b19bd35ad3b8b0710bdbbed1e2ec4b2e40fc86bc
alex <alex.me.jones@gmail.com>
2015-06-20 22:40:47 +0100

    ONE def, returns dict of name : seq

--- read_FASTA.py
+++ read_FASTA.py
@@ -2,19 +2,14 @@
 
 from pprint import pprint
 
-def read_FASTA(FASTAfile):
-    ''' creates a tuple for each protein from a FASTA file'''
-    with open (FASTAfile) as in_file:
-        return (in_file.read().split('>') [1:])
-        
-def read_FASTA_split(FASTAfile):
-    '''start using comprehension to split fasta entries '''
-    return [entry.partition('\n') for entry in read_FASTA(FASTAfile)] 
 
 def read_FASTA_protein(FASTAfile):
-    '''To end up with a clean two element list of name and sequence '''
-    return {name : seq.replace('\n','') for name, ignore, seq in \
-    read_FASTA_split(FASTAfile)}
+    ''' making one def for reading FASTA sequences into a dict with protein name as key'''
+    with open (FASTAfile) as in_file:    
+        return {part[0]: part[2].replace('\n','') for part in [record.partition('\n') for \
+        record in in_file.read().split('>')[1:]]}
+
     
 proteins = read_FASTA_protein('seq.txt')
-print (proteins['prot2'])
+#pprint (proteins)
+print (proteins['prot1'])
