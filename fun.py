0f3a3d28bacb5c1a583d9acfe5e1ab6ed076106a
alex <alex.me.jones@gmail.com>
2015-06-20 15:14:40 +0100

    to take the site from cmd line
    import sys and use argument, and this could be easily extended to take
    the file name as argv too

--- fun.py
+++ fun.py
@@ -1,5 +1,7 @@
 #!/use/bin/env python3
 
+import sys
+
 def find_site(base_seq, site):
     return (base_seq.find(site))
 
@@ -7,8 +9,8 @@ with open('seq.txt', 'rt') as seq:
     #print(seq.read())
     for line in seq:
         line = line.strip()
-        site = find_site (line, '1')
+        site_position = find_site (line, sys.argv[1])
         length = len(line)
         print('line is {0}, length is {1}, site position is {2}'.format\
-        (line, length, site), sep = ' ')
+        (line, length, site_position), sep = ' ')
         #print (site)
