5682ba29146b14f02c7238fa901404f53e573656
alex <alex.me.jones@gmail.com>
2015-06-21 10:25:41 +0100

    catching no protein name, no cut
    used assert of no protein name specified
    caught exception if protein name not in dict
    caught exception if no cut site specified

--- read_cut_FASTA.py
+++ read_cut_FASTA.py
@@ -1,14 +1,17 @@
 #!/use/bin/env python3
 
 import sys
+
+assert sys.argv[1] != None, 'specify a protein name after program' 
+
 prot_name = sys.argv[1]
-site = sys.argv[2]
 
 def read_FASTA_protein(FASTAfile):
-    ''' making one def for reading FASTA sequences into a dict with protein name as key'''
+    ''' making one def for reading FASTA sequences into a dict with protein 
+    name as key'''
     with open (FASTAfile) as in_file:    
-        return {part[0]: part[2].replace('\n','') for part in [record.partition('\n') for \
-        record in in_file.read().split('>')[1:]]}
+        return {name: sequence.replace('\n','') for name, ignore, sequence in \
+        [record.partition('\n') for record in in_file.read().split('>')[1:]]}
 
 def cut_seq(base_seq, site, offset=0):
     '''Cuts sequence into two pieces using argv[2] and returns them in a tuple '''
@@ -20,6 +23,18 @@ def cut_seq(base_seq, site, offset=0):
  
     
 proteins = read_FASTA_protein('seq.txt')
-cutting = cut_seq(proteins[prot_name], site)
+try:
+    proteins[prot_name]
+except KeyError:
+    print ('Protein name not found in FASTA')
+else:
+    print (proteins[prot_name])
+    try:
+        site = sys.argv[2] 
+    except IndexError:
+        pass
+    else:
+        cutting = cut_seq(proteins[prot_name], site)
+        print (cutting)
 
-print (proteins[prot_name], cutting)
+
