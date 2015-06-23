383d86b2983c1dbd791e94c6eeee3e89a60221a3
alex <alex.me.jones@gmail.com>
2015-06-23 22:56:14 +0100

    separating refs...make file small then dict

--- list_dict_lookup.py
+++ list_dict_lookup.py
@@ -2,6 +2,7 @@
 
 ATG = []
 GOdict = {}
+file_out = open('CLC2_GO.txt', 'wt')
 
 def get_ATG(ATGfile):
     with open(ATGfile) as in_file:
@@ -10,27 +11,33 @@ def get_ATG(ATGfile):
             ATG.append(line)
         return (ATG) 
 
-def make_go_dict(go_file, locus):
+def make_go_dict(in_file, locus):
+    with open(in_file,'rt') as GO:
+        for line in GO:
+            GOline = line.split('\t')
+            GOdict.update({GOline[0]: GOline[5]})
+
+
+def make_small_go(go_file, locus):
     with open(go_file,'rt') as GO:
         for line in GO:
-            line = line.strip()
+            #line = line.strip()
             if line.startswith(locus):
-                GOline = line.split('\t')
-                GOdict.update({GOline[0]: GOline[5]})
+                print (line)
+                file_out.write(line)
             else:
                 pass
+
+
 
 #make_go_dict('miniGO.txt')
 #print (GOdict['AT2G40WIN'])
 
-get_ATG('miniATG.txt')
+get_ATG('CLC2_all_ATG.txt')
 for locus in ATG:
-    make_go_dict('miniGO.txt', locus)
-    try:
-        print (GOdict[locus])
-    except KeyError:
-        pass
+    make_small_go('GOSLIM.txt', locus)
 
 
 
 
+file_out.close()
