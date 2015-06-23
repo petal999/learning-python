b6fdfe06daaf7ead06aec1db13d7237e5d433a3d
alex <alex.me.jones@gmail.com>
2015-06-23 12:12:30 +0100

    using list items to make dir
    to make only a small dictionary from a large file

--- list_dict_lookup.py
+++ list_dict_lookup.py
@@ -10,21 +10,22 @@ def get_ATG(ATGfile):
             ATG.append(line)
         return (ATG) 
 
-def make_go_dict(go_file):
+def make_go_dict(go_file, locus):
     with open(go_file,'rt') as GO:
         for line in GO:
             line = line.strip()
-            if line.startswith('AT'):
+            if line.startswith(locus):
                 GOline = line.split('\t')
                 GOdict.update({GOline[0]: GOline[5]})
             else:
                 pass
 
-make_go_dict('miniGO.txt')
+#make_go_dict('miniGO.txt')
 #print (GOdict['AT2G40WIN'])
 
 get_ATG('miniATG.txt')
 for locus in ATG:
+    make_go_dict('miniGO.txt', locus)
     try:
         print (GOdict[locus])
     except KeyError:
