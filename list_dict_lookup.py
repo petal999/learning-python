cb1563e64e20cfda696d1588697d6bfb23b67a51
alex <alex.me.jones@gmail.com>
2015-06-23 12:06:24 +0100

    use list item to look up dict

---
+++ list_dict_lookup.py
@@ -0,0 +1,35 @@
+#!/use/bin/env python3
+
+ATG = []
+GOdict = {}
+
+def get_ATG(ATGfile):
+    with open(ATGfile) as in_file:
+        for line in in_file:
+            line = line.strip()
+            ATG.append(line)
+        return (ATG) 
+
+def make_go_dict(go_file):
+    with open(go_file,'rt') as GO:
+        for line in GO:
+            line = line.strip()
+            if line.startswith('AT'):
+                GOline = line.split('\t')
+                GOdict.update({GOline[0]: GOline[5]})
+            else:
+                pass
+
+make_go_dict('miniGO.txt')
+#print (GOdict['AT2G40WIN'])
+
+get_ATG('miniATG.txt')
+for locus in ATG:
+    try:
+        print (GOdict[locus])
+    except KeyError:
+        pass
+
+
+
+
