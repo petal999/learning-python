aaaf78535f780efd6df62a96c6a321c3a9c5c610
alex <alex.me.jones@gmail.com>
2015-07-04 13:32:47 +0100

    an if/elif works much better

--- play.py
+++ play.py
@@ -4,13 +4,12 @@ seq = ''
 
 with open('seq.txt','rt') as seq_file:
     for line in seq_file:
-        line = seq_file.readline()
-        while line and line[0] == '>':  
-            line = seq_file.readline()
-        while line and line[0] != '>':
-            seq += line
-            line = seq_file.readline()
-    print (seq)
+        if line.startswith ('>'):
+            pass
+        elif line and line[0] != '>':
+            print (line)
+
+
 
  
 
