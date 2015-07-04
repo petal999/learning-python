5c0a670fa7aac3eddee5a027172467946b7de10c
alex <alex.me.jones@gmail.com>
2015-07-04 15:23:42 +0100

    counting lines and lengths

--- play.py
+++ play.py
@@ -1,13 +1,18 @@
 #!/use/bin/env python3
 
 seq = ''
+count = 0
+length = 0
 
 with open('seq.txt','rt') as seq_file:
     for line in seq_file:
+        line = line.strip()
+        count += 1
         if line.startswith ('>'):
             pass
         elif line and line[0] != '>':
-            print (line)
+            length = line.count('')
+        print ("line {0} is seq is {1} of length {2}".format(count, line, length))
 
 
 
